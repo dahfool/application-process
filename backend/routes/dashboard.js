89655bcdaa7260be1fd6469b38574ee34d306c48
louiaR <louitiani@gmail.com>
2018-05-02 20:01:23 +0100

    Add some changes

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -11,49 +11,56 @@ let db = new sqlite.Database(filename, (err) => {
 
 // GET APPLICANT PROGRESS DATA (TABLE STEPS) 
 router.get('/:id', function (req, res, next) {
-	let sql = 'select * from steps where id = ?'; 
-	if (typeof (Number(req.params.id)) === 'number' ) {
+	let sql = 'select * from steps where applicant_id = ?'; 
+	if (Number.isInteger(Number(req.params.id))) {
  	db.all(sql, [Number(req.params.id)], (err, rows) => {
 		if(rows.length <= 0){
-			res.status(204).json({
+			res.status(404).json({
 				steps: 'This id doesn\'t exist.'
 			});
+		}else {
+			res.status(200).json({
+				data: rows
+				});
 		}
-      	res.status(200).json({
-        data: rows
- 			});
+
 	})
-};  	
+}  else {
+	res.status(200).json({
+		message: 'The ID is invalid.'
+	})
+} 	
 });
 
 // JOIN APPLICANT  TABLE TO ALL HIS STEPS THROUGH APPLICANT ID
 router.get('/applicant/:id', function (req, res, next) {
 	let sql = `SELECT fullName, email, city, tel, status, country, experience, itAccess, hearAbout, 
-	applicants.id, steps.step_status, steps.step_number, steps.url 
-	FROM steps LEFT JOIN applicants ON steps.applicant_id = applicants.id where steps.applicant_id = ?`;
-	if (Number.isInteger(Number(req.params.id))) {
-		db.all(sql, [Number(req.params.id)], (err, rows) => {
-			if(rows.length <= 0){
-				res.status(200).json({
-					message: 'Applicant has no completed any steps.'
-				})
-			}
-			res.status(200).json({
-				step: rows
+				applicants.id, steps.step_status, steps.step_number, steps.url 
+				FROM steps LEFT JOIN applicants ON steps.applicant_id = applicants.id where steps.applicant_id = ?`;
+		if (Number.isInteger(Number(req.params.id))) {
+			db.all(sql, [Number(req.params.id)], (err, rows) => {
+				if(rows.length <= 0){
+					res.status(200).json({
+						message: 'Applicant has no submitted/completed any steps.'
+					})
+				} else {
+					res.status(200).json({
+						steps: rows
+					})
+				}
 			})
-		})
-	}  else {
-		res.status(200).json({
-			message: 'The ID is invalid.'
-		})
-	}
+		}  else {
+			res.status(200).json({
+				message: 'The ID is invalid.'
+			})
+		}
 })
 
 // INSERT URL TO TABLE STEP 
 router.post('/:id', (req, res, next) => {
 	let sql = `INSERT INTO steps (applicant_id, step_number, step_status, url) VALUES (?, ?, ?, ?)`;
-	if(Number.isInteger(Number(req.params.id))) {
-		db.run(sql,[Number(req.params.id), Number(req.body.step_number), req.body.step_status, req.body.url],
+	if(Number(req.params.id)) {
+		db.run(sql, [Number(req.params.id), Number(req.body.step_number), req.body.step_status, req.body.url],
 			function(err) {
 					res.status(201).json({
 						id: this.lastID
