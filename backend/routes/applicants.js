941261543f0bd991dcb598d14cd63c7caefada07
louiaR <louitiani@gmail.com>
2018-05-02 14:59:23 +0100

    Add status code, send response to FE, if conditions to only insert relevant data

--- backend/routes/applicants.js
+++ backend/routes/applicants.js
@@ -10,7 +10,7 @@ let db = new sqlite.Database(filename, (err) => {
 });
 
 //GET ALL APPLICANTS
-router.get('/', (req,res) => {
+router.get('/', (req, res, next) => {
 	let sql = 'select * from applicants';
 	db.all(sql, [], (err, rows) => {
 		res.status(200).json({
@@ -21,34 +21,49 @@ router.get('/', (req,res) => {
 
 
 // get one applicant through his id
-router.get('/:id', function (req, res) {
+router.get('/:id', function (req, res, next) {  
 	let sql = 'select * from applicants where id = ?'; 
+	if (Number.isInteger(Number(req.params.id))) {
  	db.all(sql, [Number(req.params.id)], (err, rows) => {
-    if (typeof (Number(req.params.id)) !== 'number' || rows.length === 0) {
-      res.send('400 - BAD REQUEST');
-    } else {
-      res.status(200).json({
-        applicants: rows
- 			});
-  	};  
-  })
+		if(rows.length === 1){
+			res.status(200).json({
+				applicants: rows
+			});
+		} else {
+			res.status(404).json({
+				message: 'This applicant doesn\'t exist.'
+			})
+		} if(err){
+			res.json({
+				message: err.message
+			})
+		}	 
+	})} else {
+		res.status(404).json({
+			message: 'Page Not Found'
+		})
+	}
 });
 
-// add new applicant route
-router.post('/', (req, res) => {
+// ADD NEW APPLICANT && STEP 0
+router.post('/', (req, res, next) => {
 	const { fullName, email, city, tel, status, country, experience, itAccess, hearAbout } = req.body;
-
-	db.run(`INSERT INTO applicants
+	// check and  insert data only if status = 1 or 0
+	if(Number.isInteger(Number(status)) && (Number(status) === 1 || Number(status) ===  0)){
+		db.run(`INSERT INTO applicants
 			(fullName, email, city, tel, status, country, experience, itAccess, hearAbout)
 				VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
-					[fullName, email, city, tel, status, country, experience, itAccess, hearAbout], function(err) {
-						if (err) {
-							return res.send('400 - BAD REQUEST').status(400);
-						}
-						return res.json({
-							id: this.lastID
+					[fullName, email, city, tel, Number(status), country, experience, itAccess, hearAbout], function(err) {
+					 res.status(201).json({
+							id: this.lastID,
 					})
 	}); 
+	} else {
+		res.status(422).json({
+			status: 'This applicant is not refugee or asylum seeker!'
+	})
+	}
+
 });
 
 
