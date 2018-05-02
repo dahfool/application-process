c4cbff589e7d52d92d2fc06be5795db2813d9edc
louiaR <louitiani@gmail.com>
2018-05-02 15:51:29 +0100

    Add -status code, -send response to FE, -if conditions to only insert relevant data

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -10,35 +10,63 @@ let db = new sqlite.Database(filename, (err) => {
 });
 
 // GET APPLICANT PROGRESS DATA (TABLE STEPS) 
-router.get('/:id', function (req, res) {
-	let sql = 'select * from steps where applicant_id = ?'; 
+router.get('/:id', function (req, res, next) {
+	let sql = 'select * from steps where id = ?'; 
+	if (typeof (Number(req.params.id)) === 'number' ) {
  	db.all(sql, [Number(req.params.id)], (err, rows) => {
-    if (typeof (Number(req.params.id)) !== 'number') {
-      res.status(400).send('Bad request');
-    } else {
-      res.status(200).json({
+		if(rows.length <= 0){
+			res.status(204).json({
+				steps: 'This id doesn\'t exist.'
+			});
+		}
+      	res.status(200).json({
         data: rows
  			});
-  	};  
-  })
+	})
+};  	
 });
 
-// insert link submitted by applicant to step table
-router.post('/:id', (req, res) => {
-	db.run(`INSERT INTO steps (applicant_id, step_number, step_status, url) VALUES (?, ?, ?, ?)`,
-		[Number(req.params.id), Number(req.body.step_number), req.body.step_status, req.body.url],
-			function(err) {
-				if (err) {
-					console.log(err.message)
-					return res.send('400 - BAD REQUEST').status(400);
-				}
-				return res.json({
-					id: this.lastID
-		}) 
-})
+// JOIN APPLICANT  TABLE TO ALL HIS STEPS THROUGH APPLICANT ID
+router.get('/applicant/:id', function (req, res, next) {
+	let sql = `SELECT fullName, email, city, tel, status, country, experience, itAccess, hearAbout, 
+	applicants.id, steps.step_status, steps.step_number, steps.url 
+	FROM steps LEFT JOIN applicants ON steps.applicant_id = applicants.id where steps.applicant_id = ?`;
+	if (Number.isInteger(Number(req.params.id))) {
+		db.all(sql, [Number(req.params.id)], (err, rows) => {
+			if(rows.length <= 0){
+				res.status(200).json({
+					message: 'Applicant has no completed any steps.'
+				})
+			}
+			res.status(200).json({
+				step: rows
+			})
+		})
+	}  else {
+		res.status(200).json({
+			message: 'The ID is invalid.'
+		})
+	}
 })
 
-// Fetch all rows steps 
+// INSERT URL TO TABLE STEP 
+router.post('/:id', (req, res) => {
+	let sql = `INSERT INTO steps (applicant_id, step_number, step_status, url) VALUES (?, ?, ?, ?)`;
+	if(Number.isInteger(Number(req.params.id))) {
+		db.run(sql,[Number(req.params.id), Number(req.body.step_number), req.body.step_status, req.body.url],
+			function(err) {
+					res.status(201).json({
+						id: this.lastID
+				}) 
+			})
+		} else {
+			res.status(200).json({
+				message: 'The ID is invalid.'
+			})
+		}				
+	})
+
+// FETCH ALL ROWS FROM TABLE STEPS
 router.get('/steps/all', (req,res) => {
 	let sql = 'select * from steps';
 	db.all(sql, [], (err, rows) => {
