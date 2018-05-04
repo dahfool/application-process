fe698b7e493892029ca7d78b986cec69db4d4355
louiaR <louitiani@gmail.com>
2018-05-04 06:31:06 +0100

    Refactoring, change status code, indent,  clean up

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -3,45 +3,49 @@ const sqlite = require('sqlite3').verbose();
 
 const filename = './database/db.sqlite';
 let db = new sqlite.Database(filename, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory application database.');
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Connected to the in-memory application database.');
 });
 
 // GET APPLICANT PROGRESS DATA (TABLE STEPS) 
-router.get('/:id', function (req, res, next) {
-	let sql = 'select * from steps where applicant_id = ?'; 
-	if (Number.isInteger(Number(req.params.id))) {
- 	db.all(sql, [Number(req.params.id)], (err, rows) => {
-		res.status(200).json({
-			data: rows
+router.get('/:id', function(req, res, next) {
+	const myId = Number(req.params.id);
+	let sql = 'select * from steps where applicant_id = ?';
+	if (Number.isInteger(myId)) {
+		db.all(sql, [myId], (err, rows) => {
+			res.status(200).json({
+				data: rows
 			});
 		})
 	} else {
-		res.status(200).json({
+		res.status(422).json({
 			message: 'Please check applicant ID'
 		})
-	} 
+	}
 });
 
 
 // INSERT URL TO TABLE STEP 
 router.post('/:id', (req, res, next) => {
+	const myId = Number(req.params.id);
+	const step_numb = Number(req.body.step_number);
+	const{ step_status, url } = req.body;
 	let sql = `INSERT INTO steps (applicant_id, step_number, step_status, url) VALUES (?, ?, ?, ?)`;
-	if(Number.isInteger(Number(req.params.id))) {
-		db.run(sql, [Number(req.params.id), Number(req.body.step_number), req.body.step_status, req.body.url],
+	if (Number.isInteger(myId)) {
+		db.run(sql, [myId, step_numb, step_status, url],
 			function(err) {
-					res.status(201).json({
-						id: this.lastID
-				}) 
+				res.status(201).json({
+					id: this.lastID
+				})
 			})
-		} else {
-			res.status(422).json({
+	} else {
+		res.status(422).json({
 			message: 'Please check applicant ID'
-			})
-		}				
-	})
+		})
+	}
+})
 
 // FETCH ALL ROWS FROM TABLE STEPS
 router.get('/steps/all', (req, res, next) => {
