52e2ec0be1d5d6ef0e5125168dac2bd7b5433d6e
louiaR <louitiani@gmail.com>
2018-05-07 19:59:03 +0100

    Rename var to const

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -8,7 +8,7 @@ exports.stepsArray = [
 ];
 
 exports.ValidURL = (str) => {
-    var pattern = new RegExp('^((https?:)?\\/\\/)?'+ 
+    const pattern = new RegExp('^((https?:)?\\/\\/)?'+ 
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ 
         '((\\d{1,3}\\.){3}\\d{1,3}))'+ 
         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ 
