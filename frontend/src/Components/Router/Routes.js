8a87ee6be686c7d535a12c390b8d7111e18b034a
louiaR <louitiani@gmail.com>
2018-05-07 03:32:49 +0100

    Add route for singleStep component

--- frontend/src/Components/Router/Routes.js
+++ frontend/src/Components/Router/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import ApplicationForm from '../ApplicationForm/ApplicationForm';
 import ApplicantDashboard from '../ApplicantDashboard/ApplicantDashboard';
+import SingleStep from '../ApplicantDashboard/SingleStep'
 import AdminGeneral from '../AdminGeneral/AdminGeneral';
 import ApplicantProgress from '../ApplicantProgress/ApplicantProgress';
 import Navbar from './Navbar';
@@ -11,6 +12,7 @@ const Routes = () => (
 		<div>
 			<Navbar />
 			<Route exact path='/' component={ApplicationForm} />
+			<Route exact path='/applicant-dashboard/:id/step/:stepnumber' component={SingleStep} />
 			<Route exact path='/applicant-dashboard/:id' component={ApplicantDashboard} />
 			<Route exact path='/applicants' component={AdminGeneral} />
       <Route path='/applicants/:id' component={ApplicantProgress} />
