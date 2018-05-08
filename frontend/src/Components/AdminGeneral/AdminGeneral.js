5abfd8194fe37a75da39ba1a91bdd5c412e64f25
OlenaKashuba <amely.jazz@gmail.com>
2018-05-08 16:43:29 +0100

    Add fetch for progress

--- frontend/src/Components/AdminGeneral/AdminGeneral.js
+++ frontend/src/Components/AdminGeneral/AdminGeneral.js
@@ -14,6 +14,7 @@ class AdminGeneral extends React.Component {
 
   componentDidMount() {
     this.getListofApplicants();
+    this.getListofSteps();
   }
 
   getListofApplicants = () => {
@@ -27,28 +28,52 @@ class AdminGeneral extends React.Component {
       .catch(err => console.log(err));
   };
 
+  getListofSteps = () => {
+    fetch(`http://localhost:3001/api/dashboard/steps/all`)
+    .then(results => results.json())
+    .then(data => {
+      this.setState({
+        steps: data.steps
+      });
+    })
+    .catch(err => console.log(err));
+  };
+
   render() {
     const applicants = this.state.applicants;
     const applicantsList = applicants.map((applicant, i) => (
       <ApplicantItem
         data={applicant}
         key={i}
+        steps={this.state.steps}
+        id={applicant.id}
       />
       ));
     return (
-      <table className="applicants-table table-bordered">
-        <thead>
-          <tr>
+      <section>
+      <h5> Step Status: </h5>
+        <ul>
+          <li> <b className='status-submitted'>S</b> - Submitted - Review required</li>
+          <li> <b className='status-approved'>A</b> - Approved</li>
+          <li> <b className='status-rejected'>R</b> - Rejected - Waiting for re-submission</li>
+        </ul>
+        <table className="applicants-table table-bordered">
+          <thead>  
             <th>Full Name</th>
             <th>City</th>
             <th>Refugee</th>
             <th>Experience</th>
             <th>IT Access</th>
-            <th>Progress</th>
-          </tr>
-        </thead>
-        <tbody>{applicantsList}</tbody>
-      </table>
+            <th>Step 0 </th>
+            <th>Step 1 </th>
+            <th>Step 2 </th>
+            <th>Step 3 </th>
+            <th>Step 4 </th>
+            <th>Step 5 </th>
+          </thead>
+          <tbody>{applicantsList}</tbody>
+        </table>
+      </section>
     );
   }
 }
--- frontend/src/Components/AdminGeneral/ApplicantItem.js
+++ frontend/src/Components/AdminGeneral/ApplicantItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Status from './Status.js';
 
 const ApplicantItem = ({data}) => {
 	return(
