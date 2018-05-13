8d8fa4e31d351a59fb16cdee7fc3ffbe3e9fb45c
louiaR <louitiani@gmail.com>
2018-05-13 14:58:54 +0100

    Add boostrap classes for basic style & remove input fields

--- frontend/src/Components/ApplicantDashboard/SubmitField.js
+++ frontend/src/Components/ApplicantDashboard/SubmitField.js
@@ -4,10 +4,11 @@ import classnames from 'classnames';
 const submitField = props => {
 	return (
 		<Fragment>
-			<form onSubmit={props.submit} className="submit-url">
+			<form onSubmit={props.submit} className="form-inline">
 				<div
-					className={classnames({
-						hidden: props.status === 'Approved' || props.status === 'Submitted',
+					className={classnames(
+						"form-group",
+						{hidden: props.status === 'Approved' || props.status === 'Submitted',
 						block: props.status === 'Rejected' || props.status === undefined,
 					})}>
 					<input
@@ -15,10 +16,12 @@ const submitField = props => {
 						type="text"
 						placeholder="Add url here"
 						name="url"
+						className="form-control form-control-lg"
+                		placeholder="Paste your link here"
 						value={props.step.url}
 						onChange={props.addUrl}
 					/>
-					<button className="btn btn-secondary" type="submit">
+					<button className="btn btn-primary ml-3 btn-lg" type="submit">
 						Submit step
 					</button>
 					<small id="emailHelp" className="form-text">
