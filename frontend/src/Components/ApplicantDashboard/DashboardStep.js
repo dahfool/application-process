2a10761c7a2610db357b77217c1f150ede850d8e
louiaR <louitiani@gmail.com>
2018-05-15 03:31:05 +0100

    Change background of number depending of status

--- frontend/src/Components/ApplicantDashboard/DashboardStep.js
+++ frontend/src/Components/ApplicantDashboard/DashboardStep.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import StatusMessage from './StatusMessage';
+import classnames from 'classnames';
 
 const DashboardStep = ({
 	step,
@@ -24,15 +25,18 @@ const DashboardStep = ({
 	return (
 		<Link to={`/applicant-dashboard/${id}/step/${step.step}`}>
 			{step.step !== 0 && (
-				<div className=" card m-2 mb-5 mt-5 card-step ">
-					<div className="step-title">
-						<span className="step-number p-5 ">{step.step}</span>
-						<div className="pt-4 pl-3 step-name">
-							{' '}
-							<h4 className="card-title">
-								<span className="title-color"> {step.details}</span>{' '}
+				<div className='card m-2 mb-5 mt-5 card-step'>
+					<div className='step-title'>
+						<span className={classnames({
+              'step-approved p-5': (status === 'Approved'),
+              'step-submitted p-5': (status === 'Submitted'),
+              'step-number p-5' : (status == undefined || status === 'Rejected')
+              })}>{step.step}</span>
+						<div className='pt-4 pl-3 step-name'>
+							<h4 className='card-title'>
+								<span className='title-color'> {step.details}</span>
 							</h4>
-							<StatusMessage status={status} stepNumber={step.step} />{' '}
+							<StatusMessage status={status} stepNumber={step.step} />
 						</div>
 					</div>
 				</div>
